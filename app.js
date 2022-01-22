7d4d5f6bca3034179cd2940547b4806b8e211b2f
slav4ocom <slav4o_1@abv.bg>
2022-01-22 03:41:27 +0300

    .

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const apiKey = "12ab99a374msh86888c209888997p1c34b3jsn3c4215e0734c";
 
 function ReadTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
     rawFile.open("GET", file, true);
-    //rawFile.setRequestHeader("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com");
-    //rawFile.setRequestHeader("X-RapidAPI-Key", apiKey);
-    //rawFile.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    //rawFile.setRequestHeader("Cache-Control", "no-cache");
     rawFile.timeout = 0;
     rawFile.onreadystatechange = function () {
         if (rawFile.readyState === 4 && rawFile.status == "200") {
@@ -23,13 +18,12 @@ function SetCityData(cityName) {
     ReadTextFile("http://192.168.0.107:8080/showdata/" + cityName + ",bg",
         function (text) {
             data = JSON.parse(text);
-            //console.log(data.main.temp);
+            
             var icon = data.weather[0].icon;
             var city = document.getElementById(cityName);
             var image = city.getElementsByTagName('img')[0];
             var temp = city.getElementsByTagName('div')[0];
             temp.innerHTML = Math.round(data.main.temp);
-            //console.log(temp);
             image.setAttribute('src', 'icons/' + icon + '.png');
         }
     );
